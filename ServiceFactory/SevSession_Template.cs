c9c9a4bee66a784503011ac59ef450283db063fc
Liufengxuan <813949669@qq.com>
2018-08-05 14:07:30 +0800

    数据操作相关TT模板生成代码

---
+++ ServiceFactory/SevSession_Template.cs
@@ -0,0 +1,56 @@
+﻿ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OA.BLL;
+using OA.IBLL;
+
+
+namespace ServiceFactory
+{
+public class SevSession
+    {
+	
+	public static IActionInfoService  GetActionInfoService()
+    {
+		  return new ActionInfoService();
+    }   
+	
+	public static IBooksService  GetBooksService()
+    {
+		  return new BooksService();
+    }   
+	
+	public static IDepartmentService  GetDepartmentService()
+    {
+		  return new DepartmentService();
+    }   
+	
+	public static IKeyWordsRankService  GetKeyWordsRankService()
+    {
+		  return new KeyWordsRankService();
+    }   
+	
+	public static IR_UserInfo_ActionInfoService  GetR_UserInfo_ActionInfoService()
+    {
+		  return new R_UserInfo_ActionInfoService();
+    }   
+	
+	public static IRoleInfoService  GetRoleInfoService()
+    {
+		  return new RoleInfoService();
+    }   
+	
+	public static ISearchDetailsService  GetSearchDetailsService()
+    {
+		  return new SearchDetailsService();
+    }   
+	
+	public static IUserInfoService  GetUserInfoService()
+    {
+		  return new UserInfoService();
+    }   
+	}
+}
