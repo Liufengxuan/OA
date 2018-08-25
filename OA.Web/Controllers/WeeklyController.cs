1ba57863a88ff28cb374c32bdc4e04a396ba5eb9
Liufengxuan <813949669@qq.com>
2018-08-25 23:25:20 +0800

    前端时间线组件调整、根据某天日期获取所在一周日期、附加查询条件

--- OA.Web/Controllers/WeeklyController.cs
+++ OA.Web/Controllers/WeeklyController.cs
@@ -22,5 +22,26 @@ namespace OA.Web.Controllers
             var weeklys= weeklyService.LoadEntities(w => 1 == 1);
             return Content(Common.SerializeHelper.SerializeToString(new { weeklys = weeklys, state = 0, msg = "获取到日志" }));
         }
+
+        public ActionResult UpdateScore(Models.RateForm form)
+        {
+            
+            Model.Weekly weekly= weeklyService.LoadEntities(w => w.Id == form.Id).FirstOrDefault();
+            if (weekly == null)
+            {
+                return Content(Common.SerializeHelper.SerializeToString(new { state = 1, msg = "该篇周报已被删除！" }));
+
+            }
+            weekly.Score = form.Score;
+            weekly.Upvote = form.Upvote;
+            weekly.avgScore = form.avgScore;
+            if (weeklyService.EditEntity(weekly))
+            {
+                return Content(Common.SerializeHelper.SerializeToString(new { state = 0, msg = "评分成功！" }));
+            }
+            return Content(Common.SerializeHelper.SerializeToString(new { state = 1, msg = "评分失败！" }));
+        }
+
+      
     }
 }
---
+++ OA.Web/Models/RateForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OA.Web.Models
+{
+    public class RateForm
+    {
+        public int Id { get; set; }
+        public int SlefId { get; set; }
+
+        public int Score { get; set; }
+
+        public float avgScore { get; set; }
+
+        public string Upvote { get; set; }
+    }
+}
